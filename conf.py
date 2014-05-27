8371317ab610473b9d0eaead3cff36d6aecebad8
Matt Choplin <choplin.mat@gmail.com>
2014-05-27 15:04:56 +0100

    [CONF] remove social buttons

--- conf.py
+++ conf.py
@@ -584,6 +584,8 @@ COMMENT_SYSTEM_ID = ""
 # <!-- End of social buttons -->
 # """
 
+SOCIAL_BUTTONS_CODE=""
+
 # Show link to source for the posts?
 # Formerly known as HIDE_SOURCELINK (inverse)
 # SHOW_SOURCELINK = True
