f9c9bc3751a51adf9c650a2e07db2b7af79b2d0a
Matthieu Choplin <choplin.mat@gmail.com>
2014-06-09 22:37:48 +0100

    [AUTO PUBLISH]

--- src/conf.py
+++ src/conf.py
@@ -663,19 +663,18 @@ SOCIAL_BUTTONS_CODE=""
 # in the default template (base.tmpl).
 # (translatable)
 # BODY_END = ""
+BODY_END="""
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-#BODY_END = """
-#<script>
-#  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-#  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-#  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-#  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-#
-#  ga('create', 'UA-51720394-1', 'mchoplin.com');
-#  ga('send', 'pageview');
-#
-#</script>
-#"""
+  ga('create', 'UA-51792900-1', 'mattchoplin.com');
+  ga('send', 'pageview');
+
+</script>
+"""
 
 # The possibility to extract metadata from the filename by using a
 # regular expression.
